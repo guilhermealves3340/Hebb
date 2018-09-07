c3940065fefe4bd963da82ed4ff79904af059770
Guilherme Alves <guilherme.alves3340@gmail.com>
2018-09-07 00:49:41 -0300

    Evoluindo

--- hebb.py
+++ hebb.py
@@ -1,10 +1,18 @@
 # Guilherme Alves
 
+"""
+    Trainamento do um neurônio 
+
+"""
+
+# Inputs
 x1 = [-1, 1, -1, 1]
 x2 = [-1, -1, 1, 1]
 
+# Output
 t = [-1, -1, -1, 1]
 
+# Deltas
 w1v = []
 w2v = []
 bv = []
@@ -19,6 +27,7 @@ for i in x2:
     w2v.append(i * t[k])
     k = k + 1
 
+# Pesos
 w1 = []
 w2 = []
 b = []
@@ -27,6 +36,7 @@ w1.append(w1v[0])
 w2.append(w2v[0])
 b.append(t[0])
 
+# Calculando os pesos
 k = 0
 while k != 3:
     w1.append(w1v[k+1] + w1[k])
@@ -34,7 +44,7 @@ while k != 3:
     b.append(b[k] + t[k+1])
     k = k+1
 
-
+# Printando os pesos após o treinamento
 print(w1[3])
 print(w2[3])
 print(b[3])
